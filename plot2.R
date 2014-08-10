cf6b4d042d72b40bdbbf00c56044e4914838a94f
Daron-Wan <Daron-Wan@users.noreply.github.com>
2014-08-11 05:16:36 +0800

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,3 +1,4 @@
+# “星期四”，“星期五”，“星期六” are chinese charactes for Thu,Fri,Sat respectively.
 consumption <- read.table("household_power_consumption.txt",sep=";",
                           header=TRUE,na.strings="NA",stringsAsFactors=FALSE)
 mydata1 <- subset(consumption,Date=="1/2/2007"|Date=="2/2/2007")
